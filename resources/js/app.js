ed79fd5bf6916204e12a3c629ff7b6d62c7aa886
unknown <cardfaux@tutanota.com>
2020-05-30 09:59:39 -0500

    vue not loading

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
-require('./bootstrap');
+require("./bootstrap");
+
+import VueRouter from "vue-router";
+import router from "./routes";
+
+window.Vue = require("vue");
+Vue.component(
+    "example-component",
+    require("./components/ExampleComponent.vue").default
+);
+
+Vue.use(VueRouter);
+
+const app = new Vue({
+    el: "#app",
+    router
+});
